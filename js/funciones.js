7f14f7a4b0e9139ae132c17ad00c5f731e5c3246
Emilio-Rendon-Dinen <61218931+Emilio-Rendon-Dinen@users.noreply.github.com>
2021-09-22 19:35:36 -0500

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,8 @@
+function agregar1() {
+  document.getElementById("nombre").innerHTML = prompt("Dime tu nombre", "");
+  document.getElementById("relacion").innerHTML = prompt("Dime tu relacion", "");
+}
+function agregar2() {
+  document.getElementById("equipo").innerHTML = prompt("Dime tu equipo", "");
+}
+//EMILIO RENDON
